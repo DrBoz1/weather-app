f70adc5b08fc01569ec9f97d391da38c4fe31865
Dr.Boz <hesamstark99@gmail.com>
2025-04-09 23:50:48 +0800

    Added the basics like the input and a search button as well as a useState hook to store the city name.

--- weather-app/src/WeatherApp.jsx
+++ weather-app/src/WeatherApp.jsx
@@ -1,7 +1,19 @@
 import React, {useState, useEffect} from 'react';
 
 function WeatherApp(){
+    const [cityName, setCityName] = useState("Malaysia");
 
+
+    return(
+        <>
+            <div className="search-area">
+                <input type="text" placeholder="Type a City Name..."/>
+                <button className="search">🔍</button>
+            
+            </div>
+        
+        </>
+    )
 }
 
 
