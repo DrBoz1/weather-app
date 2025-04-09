f6c63262044ebad7159dc6c089411cd434b8b0e4
Dr.Boz <hesamstark99@gmail.com>
2025-04-10 00:36:55 +0800

    Added a function to fetch weather data

--- weather-app/src/WeatherApp.jsx
+++ weather-app/src/WeatherApp.jsx
@@ -2,7 +2,21 @@ import React, {useState, useEffect} from 'react';
 
 function WeatherApp(){
     const [cityName, setCityName] = useState("Malaysia");
+    const [weatherData, setWeatherData] = useState(null);
 
+    async function fetchWeatherData(){
+        const apiKey = "2074262ee1d5d79dd6e2e091469c8a4e";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+
+        try{
+            const response = await fetch(url);
+            const data = await response.json();
+            setWeatherData(data);
+        }catch(error){
+            console.error("Error fetching weather data :(", error);
+        }
+    }
 
 
 
@@ -15,9 +29,11 @@ function WeatherApp(){
                 <input type="text" 
                 placeholder="Type a City Name..."
                 value={cityName} onChange={(e) => setCityName(e.target.value)}/>
-                <button className="search">🔍</button>
+                <button className="search" onClick={fetchWeatherData}>🔍</button>
             
             </div>
+
+            
         
         </>
     )
